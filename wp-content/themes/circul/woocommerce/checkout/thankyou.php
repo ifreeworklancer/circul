e3b61241be61f035e35163930b20cb6edfb70329
MaksFisher <adriel.black666@gmail.com>
2019-05-15 17:15:31 +0300

    #thank you page

--- wp-content/themes/circul/woocommerce/checkout/thankyou.php
+++ wp-content/themes/circul/woocommerce/checkout/thankyou.php
@@ -20,7 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<div class="woocommerce-order">
+<section class="thank-you-section">
 
 	<?php if ( $order ) : ?>
 
@@ -37,7 +37,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 		<?php else : ?>
 
-			<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', __( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); ?></p>
+
+			<h1 class="cart__heading"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', __( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); ?></h1>
+
 
 			<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
 
@@ -74,8 +76,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 		<?php endif; ?>
 
-		<?php do_action( 'woocommerce_thankyou_' . $order->get_payment_method(), $order->get_id() ); ?>
-		<?php do_action( 'woocommerce_thankyou', $order->get_id() ); ?>
 
 	<?php else : ?>
 
@@ -83,4 +83,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<?php endif; ?>
 
-</div>
+</section>
