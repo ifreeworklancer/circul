0668a989021904f809bfce52b1c0096220ff8d31
ifreeworklancer <ifreeworklancer@gmail.com>
2019-07-05 13:41:10 +0300

    edit category

--- wp-content/themes/circul/template-parts/categories.php
+++ wp-content/themes/circul/template-parts/categories.php
@@ -7,8 +7,10 @@
             <?php
             $prod_cat_args = array(
                 'taxonomy' => 'product_cat',
-                'hide_empty' => false, // скрывать категории без товаров или нет
-                'exclude' => 15
+                'orderby' => 'slug',
+                'number' => 7,
+                'hide_empty' => false,
+                'exclude' => 15,
             );
             $count = -1;
             $woo_categories = get_categories($prod_cat_args);
@@ -20,32 +22,34 @@
                     $woo_cat_description = $woo_cat->description; //category description
                     $category_thumbnail_id = get_woocommerce_term_meta($woo_cat_id, 'thumbnail_id', true);
                     $thumbnail_image_url = wp_get_attachment_url($category_thumbnail_id);
-
-            ?>
-            <li class="products__item <?php if ($count == 0 || $count == 1) {
-                echo 'products__item--light';
-            } elseif ($count == 2) {
-                echo 'products__item--dark products__item--discover products__item--name';
-            } elseif ($count == 3 || $count == 4 || $count == 5) {
-                echo 'products__item--light products__item--name';
-            } elseif ($count == 6) {
-                echo 'products__item--dark products__item--name';
-            } ?>">
-                <a href="<?= get_term_link($woo_cat_id, 'product_cat'); ?>" class="products__link">
-                    <div class="products__image"
-                         style="background-image: url('<?= $thumbnail_image_url ?>');"></div>
-                    <p class="products__name">
-                        <?php if ($woo_cat_description) {
-                            echo $woo_cat_description;
-                        }
-                        ?>
-                        <span class="products__highlight">
+                    $show_main_page = get_field('show_main_page', 'product_cat_' . $woo_cat_id);
+                    ?>
+                    <?php if ($show_main_page) : ?>
+                    <li class="products__item <?php if ($count == 0 || $count == 1) {
+                        echo 'products__item--light';
+                    } elseif ($count == 2) {
+                        echo 'products__item--dark products__item--discover products__item--name';
+                    } elseif ($count == 3 || $count == 4 || $count == 5) {
+                        echo 'products__item--light products__item--name';
+                    } elseif ($count == 6) {
+                        echo 'products__item--dark products__item--name';
+                    } ?>">
+                        <a href="<?= get_term_link($woo_cat_id, 'product_cat'); ?>" class="products__link">
+                            <div class="products__image"
+                                 style="background-image: url('<?= $thumbnail_image_url ?>');"></div>
+                            <p class="products__name">
+                                <?php if ($woo_cat_description) {
+                                    echo $woo_cat_description;
+                                }
+                                ?>
+                                <span class="products__highlight">
                     <?= $woo_cat_name ?>
                   </span>
-                    </p>
-                </a>
-            </li>
-            <?php endforeach; endif; ?>
+                            </p>
+                        </a>
+                    </li>
+                <?php endif; ?>
+                <?php endforeach; endif; ?>
         </ul>
     </div>
 </section>
