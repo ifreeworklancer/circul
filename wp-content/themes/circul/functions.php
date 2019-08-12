06897a37c821b97d8607e35f40f9a284c26c1745
Oleh Talanoff <talanov.o@gmail.com>
2019-08-12 17:28:44 +0300

    #

--- wp-content/themes/circul/functions.php
+++ wp-content/themes/circul/functions.php
@@ -397,11 +397,11 @@ add_filter('woocommerce_checkout_fields', 'custom_override_checkout_fields');
 
 function custom_override_checkout_fields($fields)
 {
-//    unset($fields['billing']['billing_address_2']);
-//    unset($fields['billing']['billing_postcode']);
-//    unset($fields['billing']['billing_company']);
-//    unset($fields['billing']['billing_state']);
-//    unset($fields['shipping']);
+    unset($fields['billing']['billing_address_2']);
+    unset($fields['billing']['billing_postcode']);
+    unset($fields['billing']['billing_company']);
+    unset($fields['billing']['billing_state']);
+    unset($fields['shipping']);
 
     return $fields;
 }
