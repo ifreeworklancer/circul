8d31a84ec25bffe14a74da7f03d034ff55820055
Talanoff <talanov.o@gmail.com>
2019-10-14 18:19:38 +0300

    #

--- wp-content/themes/circul/template-parts/intro.php
+++ wp-content/themes/circul/template-parts/intro.php
@@ -1,11 +1,11 @@
-<?php if(have_rows('sliders')) : ?>
+<?php if (have_rows('sliders')) : ?>
     <section class="promo">
         <link rel="stylesheet" href="<?= get_theme_file_uri('jQuery/slick/slick.css') ?>">
         <h2 class="visually-hidden">
             Circul shoes promotion
         </h2>
         <ul class="promo__list">
-            <?php while ( have_rows('repeater_field_name') ) : the_row(); ?>
+            <?php while (have_rows('sliders')) : the_row(); ?>
                 <li class="promo__item promo__item--1"
                     style="background-image:url('<?php the_sub_field('sliders_image')['url'] ?>');">
                     <p class="promo__slogan">
