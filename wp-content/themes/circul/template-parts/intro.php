f6d90dfc7e256ddfac5450d1d543abb9ecfc4478
Talanoff <talanov.o@gmail.com>
2019-10-14 18:22:24 +0300

    #

--- wp-content/themes/circul/template-parts/intro.php
+++ wp-content/themes/circul/template-parts/intro.php
@@ -1,13 +1,18 @@
-<?php if (have_rows('sliders')) : ?>
+<?php if (have_rows('sliders', 14)) : ?>
     <section class="promo">
         <link rel="stylesheet" href="<?= get_theme_file_uri('jQuery/slick/slick.css') ?>">
         <h2 class="visually-hidden">
             Circul shoes promotion
         </h2>
         <ul class="promo__list">
-            <?php while (have_rows('sliders')) : the_row(); ?>
+            <?php
+            while (have_rows('sliders', 14)) :
+                the_row();
+
+                $image = get_sub_field('sliders_image');
+                ?>
                 <li class="promo__item promo__item--1"
-                    style="background-image:url('<?php the_sub_field('sliders_image')['url'] ?>');">
+                    style="background-image:url('<?php echo $image ? $image['url'] : ''; ?>');">
                     <p class="promo__slogan">
                         <?php the_sub_field('sliders_title') ?>
                     </p>
