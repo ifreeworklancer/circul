0294011fe2837c924dbd4aaa83c26fb6cf6935f6
Talanoff <talanov.o@gmail.com>
2019-10-14 18:04:26 +0300

    #

--- wp-content/themes/circul/template-parts/intro.php
+++ wp-content/themes/circul/template-parts/intro.php
@@ -1,16 +1,20 @@
-<?php $sliders = get_field('sliders', 14); ?>
-<section class="promo">
-    <link rel="stylesheet" href="<?= get_theme_file_uri('jQuery/slick/slick.css')?>">
-    <h2 class="visually-hidden">
-        Circul shoes promotion
-    </h2>
-    <ul class="promo__list">
-        <?php foreach ($sliders as $slider) : ?>
-            <li class="promo__item promo__item--1" style="background-image:url('<?= $slider['sliders_image']['url']?>');">
-                <p class="promo__slogan">
-                   <?= $slider['sliders_title']?>
-                </p>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-</section>
+<?php $slides = get_field('sliders', 14); ?>
+
+<?php if ($slides) : ?>
+    <section class="promo">
+        <link rel="stylesheet" href="<?= get_theme_file_uri('jQuery/slick/slick.css') ?>">
+        <h2 class="visually-hidden">
+            Circul shoes promotion
+        </h2>
+        <ul class="promo__list">
+            <?php foreach ($slides as $slide) : ?>
+                <li class="promo__item promo__item--1"
+                    style="background-image:url('<?= $slide['sliders_image']['url'] ?>');">
+                    <p class="promo__slogan">
+                        <?= $slide['sliders_title'] ?>
+                    </p>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </section>
+<?php endif;
